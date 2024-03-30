309efd59694878fa8fdf79cf028ebb1cded6d4bb
Dells <jv.tandazom@uea.edu.ec>
2024-03-30 16:34:17 -0500

    Tarea completa

---
+++ my_notes.txt.py
@@ -0,0 +1,14 @@
+# Escritura de Archivo de Texto:
+
+with open("my_notes.txt", "w") as file:
+    file.write("Mis notas personales:\n")
+    file.write("- Mi nombre es Jonathan Tandazo.\n")
+    file.write("- Tengo 25 años de edad.\n")
+    file.write("- Estudio en la mejor universidad del Ecuador.\n")
+
+# Lectura de Archivo de Texto:
+
+with open("my_notes.txt", "r") as file:
+    print("Contenido del archivo my_notes.txt:")
+    for line in file.readlines():
+        print(line.strip())
